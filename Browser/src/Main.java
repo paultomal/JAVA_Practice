5d33409a3b161b1458c152b6233b377b44f4ce2a
Paul Tamal <94731937+paultomal@users.noreply.github.com>
2023-08-17 10:53:20 +0600

    Browser Delete

--- Browser/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        var browser = new Browser();
-        browser.navigate("dfjsjf");
-        System.out.println(browser);
-    }
-}
